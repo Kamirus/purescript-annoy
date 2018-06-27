06a47e15d75fbca6ebaaa584163f7dab6295b94c
Kamirus <listopadkamil@gmail.com>
2018-06-27 16:03:39 +0200

    annoy type with typelevel size

--- src/Annoy.purs
+++ src/Annoy.purs
@@ -1,1 +1,19 @@
 module Annoy where
+
+import Prelude
+
+import Data.Foldable (class Foldable)
+
+
+foreign import data Annoy :: Type -> Type
+
+
+-- fromVectors
+--   :: forall t s
+--    . Foldable t
+--   => Nat s
+--   => t (Vec s Number)
+--   -> Annoy s
+-- fromVectors = ...
+
+
