987dba33f5881468b363cda6723bc500e74eb4ec
Kamirus <listopadkamil@gmail.com>
2018-06-28 12:22:15 +0200

    get returns vec

--- src/Annoy.purs
+++ src/Annoy.purs
@@ -1,26 +1,39 @@
-module Annoy where
+module Annoy
+  ( Annoy
+  , get
+  , length
+  ) where
 
 import Prelude
 
-import Annoy.Unsafe (getNItems, unsafeGetItem)
-import Control.Monad.Eff (runPure)
-import Control.Monad.ST (runST)
+import Annoy.Unsafe (STAnnoy, getNItems, unsafeGetItem)
+import Control.Monad.Eff (Eff, runPure)
+import Control.Monad.ST (ST, runST)
 import Data.Foldable (class Foldable)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), fromJust)
 import Data.Typelevel.Num (class Lt, class Nat, toInt)
+import Data.Vec (Vec, fromArray)
+import Partial.Unsafe (unsafePartial)
 import Unsafe.Coerce (unsafeCoerce)
+
+toPrim :: forall s h. Nat s => Annoy s -> STAnnoy h
+toPrim = unsafeCoerce
+
+unsafeFromArray :: forall a s. Nat s => Array a -> Vec s a
+unsafeFromArray arr = unsafePartial $ fromJust $ fromArray arr
 
 -- | `Annoy s` where `s` is size of vectors
 foreign import data Annoy :: Type -> Type
 
 get
   :: forall s
-   . Int
+   . Nat s
+  => Int
   -> Annoy s
-  -> Maybe (Array Number)
+  -> Maybe (Vec s Number)
 get i a = 
   if 0 <= i && i < length a
-  then Just $ runPure (runST (unsafeGetItem i $ unsafeCoerce a))
+  then Just $ unsafeFromArray $ runPure (runST (unsafeGetItem i $ toPrim a))
   else Nothing
 
 length
